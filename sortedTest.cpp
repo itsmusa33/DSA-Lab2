702df6bc0e5edffcdfe1e5447c896353e455a5af
Musa <musa20042001@gmail.com>
2026-09-18 23:53:50 +0500

    Implement isSorted function

--- sortedTest.cpp
+++ sortedTest.cpp
@@ -2,7 +2,14 @@
 #include <cassert>
 using namespace std;
 
-bool isSorted(const int* arr, const int size);
+bool isSorted(const int* arr, const int size) {
+    for (int i = 0; i < size - 1; i++) {
+        if (arr[i] > arr[i + 1]) {
+            return false;
+        }
+    }
+    return true;
+}
 
 void testSortedArray(){
     int arr[] = { 1, 2, 3, 4, 5 };
